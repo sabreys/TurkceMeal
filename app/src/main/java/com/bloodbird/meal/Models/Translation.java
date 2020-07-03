0c6ef21c763d8239d3526a06f0f09bcbd32d6dac
Sabrey <trinityforce37@gmail.com>
2020-07-03 03:06:25 +0300

    yazar seçimi eklendi.local veritabanı değiştirilecek

--- app/src/main/java/com/bloodbird/meal/Models/Translation.java
+++ app/src/main/java/com/bloodbird/meal/Models/Translation.java
@@ -3,5 +3,6 @@ package com.bloodbird.meal.Models;
    public class Translation {
     public int id;
     public  Author author;
-     public String text,footnotes;
+     public String text;
+       public Footnote[] footnotes;
 }
